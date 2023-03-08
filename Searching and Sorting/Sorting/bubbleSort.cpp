bea0c5f1fc49a78dbee490b83feefab9f76ae614
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-08 19:51:57 +0530

    Bubble Sort
    Bubble Sort-->We run two loops,we just compare the 2 adjacent elements through each iteration,after each iteration the maximum element will get sorted at the end,hence we run the loop from j=0 to n-(i+1).
    
    Worst and Average Case Time Complexity is O(n^2).
    
    Best Case Time Complexity,if it is sorted array will be O(n).This is an optimnised bubble sort approach.
    We will check in the first iteration if any swap is taking place. If the array is already sorted no swap will occur and we will break out from the loops.
    Thus the iteration of the outer loop will be just 1. And our overall time complexity will be O(N).

--- Searching and Sorting/Sorting/bubbleSort.cpp
+++ Searching and Sorting/Sorting/bubbleSort.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 void bubbleSort(vector<int>&arr){
     int n=arr.size();
-    for(int i=0;i<n-1;i++){
+    for(int i=1;i<=n-1;i++){
         int didSwap=0;
         for(int j=0;j<=n-(i+1);j++){
             if(arr[j]>arr[j+1]){
